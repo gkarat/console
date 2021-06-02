9ce9c27c7c684265aae1940c0d717f29e2cdbab9
Aleksandr Voznesenskii <avoznese@redhat.com>
2021-06-02 19:06:07 +0200

    first tests to make snapshots

---
+++ frontend/packages/insights-plugin/__tests__/insightswidget.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+import { useTranslation } from 'react-i18next';
+
+import { InsightsPopup } from '../src/components/InsightsPopup/index';
+
+describe('InsightsPopup ', () => {
+  const props = {
+    PrometheusHealthPopupProps: {
+      responses: {
+        container: 'insights-operator',
+        endpoint: 'https',
+        instance: '10.129.0.5:8443',
+        job: 'metrics',
+        metric: 'critical',
+        namespace: 'openshift-insights',
+        pod: 'insights-operator-c7877d6df-4w2mf',
+        prometheus: 'openshift-monitoring/k8s',
+        service: 'metrics',
+        __name__: 'health_statuses_insights',
+      },
+      error: false,
+    },
+    i: useTranslation(),
+    metrics: { critical: 0, important: 0, low: 0, moderate: 1 },
+    clusterID: `c9b69e7e-3970-494e-ad09-6d57faad7daf`,
+    isWaitingOrDisabled: false,
+    isError: false,
+  };
+  const wrapper = shallow(<InsightsPopup {...props} />);
+  it('main container renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('InsightsPopup ', () => {
+  const props = {
+    i: useTranslation(),
+    metrics: { critical: 0, important: 0, low: 0, moderate: 1 },
+    clusterID: `c9b69e7e-3970-494e-ad09-6d57faad7daf`,
+    isWaitingOrDisabled: false,
+    isError: true,
+  };
+  const wrapper = shallow(<InsightsPopup {...props} />);
+  it('render the correct popup if response status is isError ', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('InsightsPopup ', () => {
+  const props = {
+    i: useTranslation(),
+    metrics: { critical: 0, important: 0, low: 0, moderate: 1 },
+    clusterID: `c9b69e7e-3970-494e-ad09-6d57faad7daf`,
+    isWaitingOrDisabled: true,
+    isError: false,
+  };
+  const wrapper = shallow(<InsightsPopup {...props} />);
+  it('render the correct popup if response status is isWaitingOrDisabled = true ', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
